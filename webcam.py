a5b3333d4592c8461267918dabeea81ffa73e5e0
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2020-08-11 08:23:04 +1200

    autoformatted

--- webcam.py
+++ webcam.py
@@ -1,16 +1,23 @@
 #!/bin/python3
-import requests, magic, datetime, pytz, os, json
+import requests
+import magic
+import datetime
+import pytz
+import os
+import json
 
-starttime = datetime.datetime.utcnow() - datetime.timedelta(hours=8)  #for debugging outside webcam operating hours
+starttime = datetime.datetime.utcnow()# - datetime.timedelta(hours=8) # for debugging outside webcam operating hours
 config = open("config.json", "r")
 configObj = json.loads(config.read())
 config.close()
 
 log = open(configObj["log"], "a+")
 
+
 def report(msg):
     print(msg)
     log.write(msg)
+
 
 report("\n\n----------------------------------------------\n")
 
@@ -19,11 +26,12 @@ tmpTarget = configObj["tmpTarget"] + "{}"
 finalTarget = configObj["finalTarget"] + "{}"
 dictUrl = configObj["url"]
 
+
 def getCam(url, target):
-    
+
     def getImg(_dt):
         strTime = nztz.fromutc(_dt).strftime("%y%m%d-%H%M")
-        fullUrl = url.format(t = strTime)
+        fullUrl = url.format(t=strTime)
         report("\nLooking for image at " + fullUrl)
         myfile = requests.get(fullUrl)
         open(tmpTarget.format(target), "wb").write(myfile.content)
@@ -37,7 +45,7 @@ def getCam(url, target):
         dt = dt - onemin
         getImg(dt)
         loops += 1
-    
+
     if magic.from_file(tmpTarget.format(target), mime=True) == "image/jpeg":
         report("\nFound valid jpg image for {}".format(target))
         os.rename(tmpTarget.format(target), finalTarget.format(target))
@@ -45,7 +53,9 @@ def getCam(url, target):
     else:
         report("\nNo jpg images found for {}".format(target))
 
+
 for key in dictUrl:
-    getCam("{dir}{file}_{t}.jpg".format(dir = dictUrl[key], file = key, t = "{t}"), "{}.jpg".format(key))
+    getCam("{dir}{file}_{t}.jpg".format(
+        dir=dictUrl[key], file=key, t="{t}"), "{}.jpg".format(key))
 
 log.close()
